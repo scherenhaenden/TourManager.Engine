8f0ee12cb4d82ac4bb8095362a8d1298f0eaf49f
Eddie Krill <scherenhaenden@gmail.com>
2020-10-29 08:59:53 +0100

    Draft BaseApi

---
+++ TourManagerLogic/Core/Api/BaseLogicApi.cs
@@ -0,0 +1,10 @@
+using AutoMapper;
+
+namespace TourManagerLogic.Core.Api
+{
+    public class BaseLogicApi
+    {
+        private readonly MapperConfiguration _modelToDTO;
+        private readonly MapperConfiguration _DTOToContact;
+    }
+}
