f281630b6a562370b6f9cb55728c888ff6c9394d
Eddie Krill <scherenhaenden@gmail.com>
2020-11-06 12:47:42 +0100

    Update migrations

---
+++ TourManager.Data/Migrations/20201106112417_InitialV3.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TourManager.Data.Migrations
+{
+    public partial class InitialV3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tours",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Inserted = table.Column<DateTime>(nullable: false),
+                    LastUpdate = table.Column<DateTime>(nullable: false),
+                    NameOfTour = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tours", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Venues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Inserted = table.Column<DateTime>(nullable: false),
+                    LastUpdate = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    LoadIn = table.Column<DateTime>(nullable: false),
+                    CurfView = table.Column<DateTime>(nullable: false),
+                    MaxCapacity = table.Column<int>(nullable: false),
+                    Notes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Venues", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Inserted = table.Column<DateTime>(nullable: false),
+                    LastUpdate = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    ManagerId = table.Column<int>(nullable: true),
+                    Style = table.Column<string>(nullable: true),
+                    AddressId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bands", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Inserted = table.Column<DateTime>(nullable: false),
+                    LastUpdate = table.Column<DateTime>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    VenueId = table.Column<int>(nullable: true),
+                    BandId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contacts_Bands_BandId",
+                        column: x => x.BandId,
+                        principalTable: "Bands",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TouringDates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Inserted = table.Column<DateTime>(nullable: false),
+                    LastUpdate = table.Column<DateTime>(nullable: false),
+                    DateOfTour = table.Column<DateTime>(nullable: false),
+                    VenueId = table.Column<int>(nullable: true),
+                    BandId = table.Column<int>(nullable: true),
+                    TourId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TouringDates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TouringDates_Bands_BandId",
+                        column: x => x.BandId,
+                        principalTable: "Bands",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TouringDates_Tours_TourId",
+                        column: x => x.TourId,
+                        principalTable: "Tours",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TouringDates_Venues_VenueId",
+                        column: x => x.VenueId,
+                        principalTable: "Venues",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Inserted = table.Column<DateTime>(nullable: false),
+                    LastUpdate = table.Column<DateTime>(nullable: false),
+                    Country = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    Street = table.Column<string>(nullable: true),
+                    HouseNameOrNumber = table.Column<string>(nullable: true),
+                    PostalZip = table.Column<string>(nullable: true),
+                    ExtranInfo = table.Column<string>(nullable: true),
+                    ContactId = table.Column<int>(nullable: true),
+                    VenueId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Address_Contacts_ContactId",
+                        column: x => x.ContactId,
+                        principalTable: "Contacts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Address_Venues_VenueId",
+                        column: x => x.VenueId,
+                        principalTable: "Venues",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Emails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Inserted = table.Column<DateTime>(nullable: false),
+                    LastUpdate = table.Column<DateTime>(nullable: false),
+                    EmailAddress = table.Column<string>(nullable: true),
+                    ContactId = table.Column<int>(nullable: true),
+                    VenueId = table.Column<int>(nullable: true),
+                    BandId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Emails_Bands_BandId",
+                        column: x => x.BandId,
+                        principalTable: "Bands",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Emails_Contacts_ContactId",
+                        column: x => x.ContactId,
+                        principalTable: "Contacts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Emails_Venues_VenueId",
+                        column: x => x.VenueId,
+                        principalTable: "Venues",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TelefonNumbers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Inserted = table.Column<DateTime>(nullable: false),
+                    LastUpdate = table.Column<DateTime>(nullable: false),
+                    Number = table.Column<string>(nullable: true),
+                    ContactId = table.Column<int>(nullable: true),
+                    VenueId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TelefonNumbers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TelefonNumbers_Contacts_ContactId",
+                        column: x => x.ContactId,
+                        principalTable: "Contacts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TelefonNumbers_Venues_VenueId",
+                        column: x => x.VenueId,
+                        principalTable: "Venues",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VenuesToContacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Inserted = table.Column<DateTime>(nullable: false),
+                    LastUpdate = table.Column<DateTime>(nullable: false),
+                    VenueId = table.Column<int>(nullable: false),
+                    ContactId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VenuesToContacts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VenuesToContacts_Contacts_ContactId",
+                        column: x => x.ContactId,
+                        principalTable: "Contacts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_VenuesToContacts_Venues_VenueId",
+                        column: x => x.VenueId,
+                        principalTable: "Venues",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Address_ContactId",
+                table: "Address",
+                column: "ContactId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Address_VenueId",
+                table: "Address",
+                column: "VenueId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bands_AddressId",
+                table: "Bands",
+                column: "AddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bands_ManagerId",
+                table: "Bands",
+                column: "ManagerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_BandId",
+                table: "Contacts",
+                column: "BandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Emails_BandId",
+                table: "Emails",
+                column: "BandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Emails_ContactId",
+                table: "Emails",
+                column: "ContactId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Emails_VenueId",
+                table: "Emails",
+                column: "VenueId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TelefonNumbers_ContactId",
+                table: "TelefonNumbers",
+                column: "ContactId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TelefonNumbers_VenueId",
+                table: "TelefonNumbers",
+                column: "VenueId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TouringDates_BandId",
+                table: "TouringDates",
+                column: "BandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TouringDates_TourId",
+                table: "TouringDates",
+                column: "TourId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TouringDates_VenueId",
+                table: "TouringDates",
+                column: "VenueId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VenuesToContacts_ContactId",
+                table: "VenuesToContacts",
+                column: "ContactId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VenuesToContacts_VenueId",
+                table: "VenuesToContacts",
+                column: "VenueId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bands_Contacts_ManagerId",
+                table: "Bands",
+                column: "ManagerId",
+                principalTable: "Contacts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bands_Address_AddressId",
+                table: "Bands",
+                column: "AddressId",
+                principalTable: "Address",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Address_Contacts_ContactId",
+                table: "Address");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bands_Contacts_ManagerId",
+                table: "Bands");
+
+            migrationBuilder.DropTable(
+                name: "Emails");
+
+            migrationBuilder.DropTable(
+                name: "TelefonNumbers");
+
+            migrationBuilder.DropTable(
+                name: "TouringDates");
+
+            migrationBuilder.DropTable(
+                name: "VenuesToContacts");
+
+            migrationBuilder.DropTable(
+                name: "Tours");
+
+            migrationBuilder.DropTable(
+                name: "Contacts");
+
+            migrationBuilder.DropTable(
+                name: "Bands");
+
+            migrationBuilder.DropTable(
+                name: "Address");
+
+            migrationBuilder.DropTable(
+                name: "Venues");
+        }
+    }
+}
