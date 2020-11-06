139a155e3bfd8691a6c22fca181c642b314d2981
Eddie Krill <scherenhaenden@gmail.com>
2020-11-06 12:53:42 +0100

    Update models

--- TourManagerLogic/Core/Models/TouringDatesModel.cs
+++ TourManagerLogic/Core/Models/TouringDatesModel.cs
@@ -6,6 +6,7 @@ namespace TourManagerLogic.Core.Models
     public class TouringDatesModel: BaseModel
     {
         public DateTime DateOfTour  { get; set; }
-        public virtual ICollection<VenueModel> Venues { get; set; }
+        public virtual VenueModel Venue { get; set; }
+        public virtual BandModel Band { get; set; }
     }
 }
